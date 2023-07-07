b736dfb2d154c23bf0076c29f0278d5975dd4e4c
Mohammad Arshad <mdarshad1000@gmail.com>
2023-07-07 17:38:56 +0530

    Add main program file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# DPS
+# Snake Table Generator
+
+This Python project generates a snake table based on a string of integers from the user. The snake table arranges the elements of the list in a snake pattern.
+
+## Prerequisites
+
+- Python 3.9
+
+## Installation
+
+1. Clone the repository:
+
+```git clone git@github.com:mdarshad1000/DPS.git```
+
+
+2. Navigate to the project directory
+
+3. Run the main.py script:
+
+```python main.py```
+
+4. Enter a sequence of numbers when prompted
+
+5. You will receive an output
+
+
+
+
---
+++ main.py
@@ -0,0 +1,54 @@
+import math
+from typing import List
+
+
+def snakeTable(userArray: List[int]) -> None:
+    """
+    Prints a snake table based on the given userArray.
+    The snake table is created by arranging the elements of userArray in a zigzag pattern.
+
+    Args:
+        userArray (List[int]): The list of integers to be arranged in the snake table.
+
+    Returns:
+        None
+
+    Example:
+        Enter the number: 123456
+        135
+        246
+
+    """
+    size = math.ceil(len(userArray) / 2) 
+
+    # Initalize an empty 2D array
+    snakeTable = [['' for _ in range(size)] for _ in range(2)]
+    
+    flag = True
+    row = 0
+    col = 0
+
+    for i in range(len(userArray)):
+        if flag:
+            snakeTable[row][col] = userArray[i]
+            if row < 1:
+                row += 1
+            else:
+                row -= 1
+            flag = False
+        else:
+            snakeTable[row][col] = userArray[i]
+            col += 1
+            flag = True
+
+    for row in snakeTable:
+        print(''.join(str(element) for element in row))
+
+
+# Take user input as a string
+userInput = input("Enter the number: ")
+
+# Convert the string into a list
+userArray = list(map(int, userInput))
+
+snakeTable(userArray)
